c8e95ac1dbb2e0c18fad3be6322fae78ec7abf9f
Tomasz Knapik <hi@tmkn.org>
2019-04-04 14:37:11 +0100

    Add timesheet generated files to the admin panel

--- timetracker/sheets/admin.py
+++ timetracker/sheets/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
-from timetracker.sheets.models import TimeSheet
+from timetracker.sheets.models import TimeSheet, TimeSheetGeneratedFile
+
+
+class TimeSheetGeneratedFileInline(admin.TabularInline):
+    model = TimeSheetGeneratedFile
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(TimeSheet)
 class TimeSheetAdmin(admin.ModelAdmin):
     list_filter = ('user', )
     search_fields = ('title', )
+    inlines = (TimeSheetGeneratedFileInline, )
