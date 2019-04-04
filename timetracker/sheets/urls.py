7c81096d3f7bcc3798e716fbc89f7b7284275c8b
Tomasz Knapik <hi@tmkn.org>
2019-04-04 03:13:55 +0100

    Add time sheet delete view

--- timetracker/sheets/urls.py
+++ timetracker/sheets/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from timetracker.sheets.views import TimeSheetCreateView, TimeSheetListView
+from timetracker.sheets.views import (TimeSheetCreateView, TimeSheetDeleteView,
+                                      TimeSheetListView)
 
 app_name = 'sheets'
 
 urlpatterns = [
     path('', TimeSheetListView.as_view(), name='list'),
     path('create/', TimeSheetCreateView.as_view(), name='create'),
+    path('<int:pk>/delete/', TimeSheetDeleteView.as_view(), name='delete'),
 ]
